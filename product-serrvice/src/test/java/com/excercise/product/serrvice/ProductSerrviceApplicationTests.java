c685cb33cb5ca88b280570ffba49f918e74737f5
sherwind <sherwin.doan@code88.asia>
2022-12-22 15:49:59 +0700

    Initial product-service

---
+++ product-serrvice/src/test/java/com/excercise/product/serrvice/ProductSerrviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.excercise.product.serrvice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductSerrviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
